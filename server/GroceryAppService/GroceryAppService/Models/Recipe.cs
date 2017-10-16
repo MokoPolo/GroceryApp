c1091e1ade279342ddce24b326203082f37a3ca7
Marc <marc.cueto@gmail.com>
2017-10-15 23:13:30 -0400

    Add mongodb

--- server/GroceryAppService/GroceryAppService/Models/Recipe.cs
+++ server/GroceryAppService/GroceryAppService/Models/Recipe.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,15 @@ namespace GroceryAppService.Models
     {
         public string Name { get; set; }
         public List<string> Ingredients { get; set; }
+
+        public void ConnectToDB()
+        {
+            var client = new MongoClient();
+
+            IMongoDatabase db = client.GetDatabase("MarcDB");
+
+            IMongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("Recipes");
+        }
 
     }
 }
---
+++ server/GroceryAppService/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryAppService.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Recipe recipe = new Recipe();
+            recipe.ConnectToDB();
+        }
+    }
+}
