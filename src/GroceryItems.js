08a0361727de17cce7025cbf2ce2efd13576fd66
Marc <marc.cueto@gmail.com>
2017-10-29 18:43:17 -0400

    Weekends work

--- src/GroceryItems.js
+++ src/GroceryItems.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Input, ListGroup, ListGroupItem } from 'reactstrap';
+const GroceryItems = ({ toggleItemClick, ingredients }) => {
+    /*     const GroceryItems = ingredients.map((ingredient) =>
+            <li key={ingredient.Name}>{ingredient.Name}</li>
+        ); */
+    const GroceryItems2 = ingredients.map((ingredient) => {
 
-const GroceryItems = ({ ingredients }) => {
-    const GroceryItems = ingredients.map((ingredient) =>
-        <li key={ingredient.Name}>{ingredient.Name}</li>
-    );
+/*         let strikeclassname;
+        if (ingredient.Done === true) {
+            strikeclassname = "strike";
+        }
+        else {
+            strikeclassname = "nostrike";
+        }
+        const itemClass = "TransparentBackground NoBorders " + strikeclassname; */
+        <ListGroupItem><Input type="checkbox" onClick={() => toggleItemClick(ingredient.Id)} /> {ingredient.Name}</ListGroupItem>
+        {/* <ListGroupItem key={ingredient.Id} className="TransparentBackground NoBorders"><Input type="checkbox" onClick={() => toggleItemClick(ingredient.Id)} /> {ingredient.Name}</ListGroupItem> */}
+    });
     return (
-        <ul>
-            {GroceryItems}
-        </ul>
+        <div>
+            <ListGroup className="TransparentBackground">
+                {GroceryItems2}
+            </ListGroup>
+        </div>
     );
 }
 
 GroceryItems.propTypes = {
-    ingredients: PropTypes.array
+    ingredients: PropTypes.array,
+    toggleItemClick: PropTypes.func
 };
 
 export default GroceryItems;
